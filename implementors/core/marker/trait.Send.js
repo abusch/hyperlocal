55bdaa6d7feca4047a15d52aaea2064d01a06dba
Travis CI User <travis@example.org>
2018-08-13 00:21:14 +0000

    Update documentation

---
+++ implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["hyperlocal"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"hyperlocal/struct.Uri.html\" title=\"struct hyperlocal::Uri\">Uri</a>&lt;'a&gt;",synthetic:true,types:["hyperlocal::Uri"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"hyperlocal/client/struct.UnixConnector.html\" title=\"struct hyperlocal::client::UnixConnector\">UnixConnector</a>",synthetic:true,types:["hyperlocal::client::UnixConnector"]},{text:"impl&lt;S&gt; !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"hyperlocal/server/struct.Server.html\" title=\"struct hyperlocal::server::Server\">Server</a>&lt;S&gt;",synthetic:true,types:["hyperlocal::server::Server"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"hyperlocal/server/struct.Http.html\" title=\"struct hyperlocal::server::Http\">Http</a>",synthetic:true,types:["hyperlocal::server::Http"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
